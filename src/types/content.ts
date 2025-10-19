321a2a9986ae24936e3694a3175e206e6d8404cc
David <dtrejot021106@gmail.com>
2025-10-19 17:47:46 -0600

    olvide la plantilla y lo hare a mano todo

--- src/types/content.ts
+++
@@ -1,7 +0,0 @@
-import type { CollectionEntry } from "astro:content";
-
-export type Blog = CollectionEntry<"about">;
-export type Note = CollectionEntry<"notes">;
-export type Bookmark = CollectionEntry<"bookmarks">;
-export type Project = CollectionEntry<"projects">;
-export type Experience = CollectionEntry<"experience">; 
--- src/types/navigation.ts
+++
@@ -1,6 +0,0 @@
-export interface NavigationItem {
-    name: string;
-    path: string;
-}
-
-export type Navigation = NavigationItem[]; 
